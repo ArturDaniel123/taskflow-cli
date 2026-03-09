f6498cbee71c38e610f8108177561834e41d3d77
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2026-03-09 01:52:47 -0300

    Ultimas atualizações

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from menu import menu
-from tarefas import tarefas
+from tarefas import criar_tarefas, listar_tarefas, excluir_tarefas
 
-print(menu())
-if menu() == '1':
-    tarefas()
+banco_de_tarefas = []
+
+def iniciar():
+    while True:
+        opcao = menu()
+        
+        if opcao == '1':
+            criar_tarefas(banco_de_tarefas)
+        elif opcao == '2':
+            listar_tarefas(banco_de_tarefas)
+        elif opcao == '3':
+            excluir_tarefas(banco_de_tarefas)
+        elif opcao == '0':
+            print("Saindo do sistema...")
+            break
+        else:
+            print("Opção inválida. Tente novamente")
+            
+if __name__ == "__main__":
+    iniciar()   
+        
+    
     
         
--- tarefas.py
+++ tarefas.py
@@ -4,13 +4,32 @@ def criar_tarefas(lista_geral):
                        "Alta, baixa ou média? ").capitalize()
     
     nova_tarefa = {
-        "tarefa" : nome,
-        "prioridade" : prioridade,
-        "status" : "Pendente"
+        "tarefa": nome,
+        "prioridade": prioridade,
+        "status": "Pendente"
     }
     
     lista_geral.append(nova_tarefa)
     print(f"{nome} salva com sucesso!")
-
+    
+def listar_tarefas(lista_geral):
+    if not lista_geral:
+        print("\n Nenhuma tarefa cadastrada")
+        return
+    
+    print("=== LISTA DE TAREFAS ===")
+    for indice, dicio in enumerate(lista_geral):
+        status_icon = "✔" if dicio['status'] == "Concluída" else "☐"
+        print(f"{indice}. [{status_icon}] {dicio['tarefa']} | Prioridade: {dicio['prioridade']}")
+    
+def excluir_tarefas(lista_geral):
+    listar_tarefas(lista_geral)
+    if lista_geral:
+        try:
+            indice = int(input("\nDigite o número da tarefa para excluir: "))
+            removida = lista_geral.pop(indice)
+            print(f"Tarefa {removida['tarefa']} removida!")
+        except (ValueError, IndexError):
+            print("Opção inválida")
 
     
