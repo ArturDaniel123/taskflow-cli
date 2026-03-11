58dc594571477bf1cf7ebeade78d9e8c4b123ba5
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2026-03-10 21:35:21 -0300

    Aprimorando projeto

--- tarefas.py
+++ tarefas.py
@@ -1,27 +1,38 @@
 import time
 
 def criar_tarefas(lista_geral):
-    nome = input("\nDigite uma tarefa: ").capitalize().strip()
-    
     while True:
-        prioridade = input("\nQual o nível de prioridade? "
-                        "Alta, baixa ou média? ").capitalize()
-        if prioridade not in ['Alta', 'Média', 'Media', 'Baixa']:
-            print(f'\n{prioridade} não é uma opção válida. Por favor, tente '
-                'novamente')
-            time.sleep(0.9)
-        else:
-            break
+        nome = input("\nDigite uma tarefa (ou tecle 'S' para sair): ").capitalize().strip()
+        if not nome:
+            print("\nEspaço vazio. Tente novamente")
+            time.sleep(1)
+            continue
+        
+        if nome.upper() == 'S':
+            print("Saindo...")
+            time.sleep(1)
+            return
+        
+        while True:
+            prioridade = input("\nQual o nível de prioridade? "
+                            "Alta, baixa ou média? ").capitalize()
+            if prioridade not in ['Alta', 'Média', 'Media', 'Baixa']:
+                print(f'\n{prioridade} não é uma opção válida. Por favor, tente '
+                    'novamente')
+                time.sleep(0.9)
+            else:
+                break
     
-    nova_tarefa = {
-        "tarefa": nome,
-        "prioridade": prioridade,
-        "status": "Pendente"
-    }
-    
-    lista_geral.append(nova_tarefa)
-    print(f"\n{nome} salva com sucesso!")
-    time.sleep(1)
+        nova_tarefa = {
+            "tarefa": nome,
+            "prioridade": prioridade,
+            "status": "Pendente"
+        }
+        
+        lista_geral.append(nova_tarefa)
+        print(f"\n{nome} salvo(a) com sucesso!")
+        time.sleep(1)
+        break
     
 def listar_tarefas(lista_geral):
     if not lista_geral:
@@ -33,7 +44,7 @@ def listar_tarefas(lista_geral):
     for indice, dicio in enumerate(lista_geral):
         status_icon = "✔" if dicio['status'] == "Concluída" else "☐"
         print(f"{indice}. [{status_icon}] {dicio['tarefa']} | Prioridade: {dicio['prioridade']}")
-        time.sleep(1)
+        time.sleep(0.2)
     
 def excluir_tarefas(lista_geral):
     if not lista_geral:
@@ -42,15 +53,24 @@ def excluir_tarefas(lista_geral):
         
     while True:
         listar_tarefas(lista_geral)
+        
+        entrada = input("\nDigite o número da tarefa para"
+                   "excluir (ou 'S' para sair):")
+        
+        if entrada.upper() == 'S':
+            print("\nSaindo da exclusão...")
+            time.sleep(1)
+            break
+        
         if lista_geral:
             try:
-                indice = int(input("\nDigite o número da tarefa para excluir: "))
+                indice = int(entrada)
                 removida = lista_geral.pop(indice)
                 print(f"\nTarefa {removida['tarefa']} removida!")
                 time.sleep(1)
                 break
             except (ValueError, IndexError):
-                print("Opção inválida. Tente novamente")
+                print("\nOpção inválida. Tente novamente")
                 time.sleep(0.9)
 
 def concluir_tarefas(lista_geral):
@@ -71,7 +91,7 @@ def concluir_tarefas(lista_geral):
             lista_geral[indice]['status'] = "Concluída"
             
             print(f"\n✅ Tarefa '{lista_geral[indice]['tarefa']}' marcada como concluída!")
-            time.sleep(1.5)
+            time.sleep(1)
             break
         
         except (ValueError, IndexError):
